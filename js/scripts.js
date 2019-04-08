5f26fb8b8de9782953e9acf746454ead481b4a77
Brendan Hellar and Stephen Trewick <stephen@>
2019-04-08 09:18:21 -0700

    show address boook with minimal user interface features

---
+++ js/scripts.js
@@ -0,0 +1,69 @@
+//Business Logic for AddressBook-----
+  function AddressBook() {
+    this.contacts = [],
+    this.currentId = 0
+  }
+
+  AddressBook.prototype.addContact = function(contact) {
+    contact.id= this.assignId();
+    this.contacts.push(contact);
+    }
+  AddressBook.prototype.assignId = function() {
+    this.currentId += 1;
+    return this.currentId;
+  }
+  AddressBook.prototype.findContact = function(id) {
+    for (var i=0; i<this.contacts.length; i++){
+      if (this.contacts[i]){
+        if (this.contacts[i].id == id) {
+          return this.contacts[i];
+        }
+      }
+    };
+    return false;
+  }
+  AddressBook.prototype.deleteContact = function(id) {
+    for (var i=0; i<this.contacts.length; i++) {
+      if (this.contacts[i]) {
+        if (this.contacts[i].id == id) {
+          delete this.contacts[i];
+          return true;
+        }
+      }
+    };
+  return false;
+}
+
+// Business Logic for Contacts ---------
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName,
+  this.lastName = lastName,
+  this.phoneNumber = phoneNumber
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " "+this.lastName;
+}
+//User interface logic
+var addressBook = new AddressBook();
+
+function displayContactDetails(addressBookToDisplay) {
+  var contactsList = $("ul#contacts");
+  var htmlForContactInfo = "";
+  addressBookToDisplay.contacts.forEach(function(contact) {
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+};
+
+$(document).ready(function(){
+  $("form#new-Contact").submit(function(event){
+    event.preventDefault();
+    var inputtedFirstName = $("input#newFirst").val();
+    var inputtedLastName = $("input#newLast").val();
+    var inputtedPhoneNumber = $("input#newNumber").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    addressBook.addContact(newContact);
+    displayContactDetails(addressBook);
+  })
+})
